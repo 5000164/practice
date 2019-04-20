6418ad9dbc23e8e6d14a3e224bb9f0b81af230fa
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-04-21 00:41:38 +1000

    feat: to show Fibonacci numbers

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fibonacci = require('./fibonacci');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -6,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.end([...Array(10).keys()].map(i => i + 1).map(fibonacci).join(', ') + '\n');
 });
 
 server.listen(port, hostname, () => {
