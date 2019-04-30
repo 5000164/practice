6db63d88c83ed881200220560e6657c2ae0cdfda
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-04-30 19:01:26 +1000

    chore: to check that I can use a native JavaScript function

--- scalajs/src/main/scala/interfaces/Application.scala
+++ scalajs/src/main/scala/interfaces/Application.scala
@@ -1,7 +1,9 @@
 package interfaces
 
+import scala.scalajs.js
+
 object Application {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println(js.Dynamic.global.window.location.origin)
   }
 }
